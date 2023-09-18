09b2ea7dcbc2d5578343ee3b0d291193b34efea0
Dara Ajayi <dara@getreallabs.com>
2023-09-18 18:33:00 -0400

    Change naming from gpu to cuda

--- run.cuda.sh
+++ run.cuda.sh
@@ -1,6 +1,6 @@
-docker build . -f Dockerfile.gpu -t facefusion-gpu
+docker build . -f Dockerfile.cuda -t facefusion-cuda
 docker run -it --rm \
     --gpus all \
     -p 8080:7860 \
     -v ~/ML/facefusion/.assets/:/facefusion/.assets/ \
-    facefusion-gpu $1
+    facefusion-cuda $1
