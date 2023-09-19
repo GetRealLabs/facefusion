659d4e2373f2ef17350ef91f0c31018d36799f34
Chris Powers <cpowers@outlook.com>
2023-09-19 09:24:54 -0500

    Fix run scripts

--- run.cpu.sh
+++ run.cpu.sh
@@ -2,6 +2,8 @@ docker build . -f Dockerfile.cpu -t facefusion-cpu
 docker run -it --rm \
     -p 8080:7860 \
     -v ~/ML/facefusion/.assets/:/facefusion/.assets/ \
-    facefusion-cpu $1
+    -v ~/ML/facefusion/root/:/root/ \
+    -v ~/ML/media/:/facefusion/media/ \
+    facefusion-cpu "$@"
    
-# -s ./media/source.jpeg -t ./media/target.png -o ./media/output.png
+# -s /facefusion/media/source.jpeg -t /facefusion/media/target.mp4 -o /facefusion/media/output.mp4
--- run.cuda.sh
+++ run.cuda.sh
@@ -3,4 +3,8 @@ docker run -it --rm \
     --gpus all \
     -p 8080:7860 \
     -v ~/ML/facefusion/.assets/:/facefusion/.assets/ \
-    facefusion-cuda $1
+    -v ~/ML/facefusion/root/:/root/ \
+    -v ~/ML/media/:/facefusion/media/ \
+    facefusion-cuda "$@"
+
+# -s /facefusion/media/source.jpeg -t /facefusion/media/target.mp4 -o /facefusion/media/output.mp4
